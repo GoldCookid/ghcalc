ef73dc4360f788aede0745736e5ee9cedaeb427e
GoldCookid <94555265+GoldCookid@users.noreply.github.com>
2021-11-17 19:05:42 +0200

    Update ghcalc.py

--- ghcalc.py
+++ ghcalc.py
@@ -1,38 +1,111 @@
 i = 1
 
-while i == 1:
-	print("калькулятор")
+while i == 1: #працює	
+
+	print ("-_-_-_-ПРОДУКТОВИЙ-_-_-_-")
 	print()
-	num1 = float(input("Ведіть перше чисо:  "))
-	num2 = float(input("Ведіть друге чисо:  "))
-	print("Операції")
+
 	print()
-	print("1 - додавання")
-	print("2 - віднімання")
-	print("3 - множення")
-	print("4 - ділення")
+	print("1 - овочі")
+	print("2 - фрукти")
+	print("3 - все разом")
 	print()
-	vybir2 = float(input('Zrobit vybir:  '))
+	a = int(input('виберіть що ви взяли:  '))
 	print()
-	if vybir2 == 1:
-		rezult = num1 + num2
-		print("додавання dvoch:  " + srt(rezult))
-	elif vybir2 == 2:
-		rezult = num1 - num2
-		print ("віднімання:  " + str(rezult))
-	elif vybir2 == 3:
-		rezult = num1 * num2
-		print("додавання:  " + str(rezult))
-	elif vybir2 == 4:
-		rezult = num1 / num2
-		rint("ділення:  " + str(rezult))
-		print()
-	print("1 - продовжити")
-	print("2 - завершити ")
-	
-	vubir = int(input('Зробіть свій вибір: '))
+	if a == 1:
+		h = 1
+		while h == 1:
+			print("1кг = 30 грн")
+			print("більше 100кг знишка 0.05%")
+			print()
+			b = int(input("Ведіть скільки ви взяли в кг:"))
+			print()
+			if b <= 100:
+				print(b)
+				print("знишки нема")
+				print()
+				znushka = b * 30 
+				print("чек: грн " + str(round(znushka,2)))
+				print("без знишки")
+			elif b > 100:
+				print(b)
+				print("ви взяли")
+				print()
+				print("разом зi знишко виходить")
+				c = b * 100
+				znushka = b * 30 * 0.05
+				suma = c - znushka
+				print ("чек: грн " + str(round(suma,2)))
+				print("разом зi знишко")
+			print()
+			print("1 - продовжитив використання")
+			print("0 - завершити використання")
+			h = int(input("Зробіть вибір:"))
+
+	if a == 2:
+		h = 1
+		while h == 1:	
+			print("1кг = 150 грн")
+			print("більше 100кг знишка 0.07%")
+			print()
+
+			b = int(input("Ведіть скільки ви взяли в кг:"))
+			print()
+			if b < 150:
+				print(b)
+				print("знишки нема")
+				print()
+				znushka = b * 150 
+				print("чек: грн " + str(round(znushka,2)))
+				print("без знишки")
+			elif b >= 150:
+				print(b) 
+				print("ви взяли")
+				print()
+				print("разом зi знишко виходить")
+				c = b * 150 
+				znushka = b * 150 * 0.07
+				suma = c - znushka	
+				print ("чек: грн " + str(round(suma,2)))
+				print("разом зi знишко")
+			print()
+			print("1 - продовжитив використання")
+			print("0 - завершити використання")
+			h = int(input("Зробіть вибір:"))
+	if a == 3:
+		h = 1
+		while h == 1:
+			print("1кг = 180 грн")
+			print("більше 200кг знишка 0.12%")
+			print()
+
+
+			b = int(input("Ведіть скільки ви взяли в кг:"))
+			print()
+			if b <= 200:
+				print(b)
+				print("знишки нема")
+				print()
+				znushka = b * 180 
+				print("чек: грн " + str(round(znushka,2)))
+			elif b >= 200:
+				print(b)
+				print("ви взяли")
+				print()
+				print("разом зi знишко виходить")
+				print()
+				c = b * 180
+				znushka = b * 180 * 0.12
+				suma = c - znushka
+
+				
+				print ("чек: грн " + str(round(suma,2)))
+
+
+			print("1 - продовжитив використання")
+			print("0 - завершити використання")
+			h = int(input("Зробіть вибір:"))
 	print()
-	if vubir == 1:
-		i = 1 
-	elif vubir == 2:
-		i = 0				
+	print("1 - почати заново")
+	print("0 - вийти")
+	i = int(input("Зробіть вибір:"))
